e44f43a49e028ec91d7e7857e292bee47a7d54cc
wangxuefeng <xuefeng.wang@ctrip.com>
2019-04-10 14:04:11 +0800

    test fix

--- app/renderer/containers/App.test.tsx
+++ app/renderer/containers/App.test.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import {configureStore, history} from '../store';
+import {ConnectedRouter} from 'connected-react-router';
+import {Provider} from 'react-redux';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const store = configureStore();
+  ReactDOM.render(<Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
